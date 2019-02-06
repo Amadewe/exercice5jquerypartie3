342b699e732ecb708e3e32da1730da25fd91ab99
Amadewe <dewevre.amandine@gmail.com>
2019-02-06 16:29:32 +0100

    Correction, première partie

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,36 @@
 $(function(){
-  $('#target').keydown(function(event){
-    // mettre mes var dans la fonction !!!!!!!!!
-    // position initiale de mon carré en haut à gauche
-    var positionX = $('.square').position().left;
-    var positionY = $('.square').position().top;
-    // retour à la taille de ma fenetre
-    var widthWindow = $(window).width();
-    var heightWindow = $(window).height();
+  //On stocke la largeur et la hauteur de la fenêtre dans des variables
+  var windowWidth = $(window).width();
+  console.log('Largeur de fenêtre : ' + windowWidth + '.');
+  var windowHeight = $(window).height();
+  console.log('Hauteur de fenêtre : ' + windowHeight + '.');
 
-    if (event.which == 37) {  // fleche gauche ok
-      if (positionX < 0){
-        $('.square').css('left', widthWindow);
-      }else {
-        // mon carré bouge à gauche de 10 px
-        $('.square').animate({'left':'-=10px'});
-      }
+  //On stocke la largeur et la hauteur du carré dans des variables
+  var squareWidth = $('#square').width();
+  console.log('Hauteur du carré : ' + squareWidth + '.');
+  var squareHeight = $('#square').height();
+  console.log('Largeur du carré : ' + squareHeight + '.');
+  //Je met un évènement"keydown" sur mon Input.
+  //Je met "event" en paramètre de ma fonction anonyme, grâce à ça je vais pouvoir récupérer la touche sur laquelle j'ai appuyé.
+  $('#direction').keydown(function(event){
+    var squarePositionLeft = $('#square').offset().left;
+    var squarePositionTop = $('#square').offset().top;
+    //Switch est utilisé lorsque l'on veut comparer une variable à plusieurs valeurs
+    switch (event.keyCode) {
+      case 37:
+        squarePositionLeft -= 10;
+      break;
+      case 38:
+        squarePositionTop -= 10;
+      break;
+      case 39:
+        squarePositionLeft += 10;
+      break;
+      case 40:
+        squarePositionTop += 10;
+      break;
     }
-    if (event.which == 39) {  // fleche droite
-      if (positionX > widthWindow){
-        $('.square').css('left', 0);
-      }else {
-        // mon carré bouge à droite de 10 px
-        $('.square').animate({'left':'+=100px'});
-      }
-    }
-    if (event.which == 38) {  // fleche haut
-      if (positionY < 0){
-        $('.square').css('top', heightWindow);
-      }else {
-        // mon carré bouge à gauche de 10 px
-        $('.square').animate({'top':'-=10px'});
-      }
-    }
-    if (event.which == 40) {  // fleche bas
-      if (positionY > heightWindow){
-        $('.square').css('top', 0 );
-      }else {
-        // mon carré bouge à gauche de 10 px
-        $('.square').animate({'top':'+=100px'});
-      }
-    }
-
-    console.log(positionX);
-    console.log(positionY);
+  //On définit la position du carré grâce à la fonction "Offset".
+  $('#square').offset({top : squarePositionTop, left : squarePositionLeft});
   });
 });
-
-//  ------------------ AVEC SWITCH -----------
-// $(document).ready(function(){
-//   $('#target').keydown(function(event){
-//
-//
-// switch (event.which){
-//   case 37: // fleche gauche
-//     $('.carre').animate({'left':'-=10px'});
-// break;
-//   case 38: // fleche haut
-//     $('.carre').animate({'top':'-=10px'});
-// break;
-// case 39: // fleche droite
-//     $('.carre').animate({'left':'+=10px'});
-// break;
-// case 40: // fleche bas
-//     $('.carre').animate({'top':'+=10px'});
-// break;
-// }
-//   });
-// });
